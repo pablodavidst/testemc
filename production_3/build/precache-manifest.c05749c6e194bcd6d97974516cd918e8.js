10521148b0006ca15cfdb5bf9b69afa003616f38
pablo st <pablo.david.st@gmail.com>
2020-07-02 21:11:54 -0300

    commit build 3ra version

---
+++ production_3/build/precache-manifest.c05749c6e194bcd6d97974516cd918e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de5d6747827333f7dbdb19ff8889458b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5653eead2c9c3bb7e4f",
+    "url": "/static/css/main.181abebc.chunk.css"
+  },
+  {
+    "revision": "6f29f35e38fc15c9a480",
+    "url": "/static/js/2.41b8999d.chunk.js"
+  },
+  {
+    "revision": "c2e47fc3395753f1500a13546dd93ceb",
+    "url": "/static/js/2.41b8999d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5653eead2c9c3bb7e4f",
+    "url": "/static/js/main.defd2c46.chunk.js"
+  },
+  {
+    "revision": "c81606e8a4beff6044ae",
+    "url": "/static/js/runtime-main.37c091aa.js"
+  }
+]);
