e4f8b51e4e2b176581c135f5742d7ca86f33a783
pablo st <pablo.david.st@gmail.com>
2020-07-04 03:06:41 -0300

    subida de archivos build cuarta versiòn

---
+++ production_4/build/precache-manifest.0243ea502bccfad19bea8f74ba2e3fc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32757c1ae746717486e6c27ca5931cea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46e9c2caa3e6eb0e908e",
+    "url": "/static/css/main.5b50e28e.chunk.css"
+  },
+  {
+    "revision": "acba46edefa2ec902b5e",
+    "url": "/static/js/2.63fc2e3d.chunk.js"
+  },
+  {
+    "revision": "c2e47fc3395753f1500a13546dd93ceb",
+    "url": "/static/js/2.63fc2e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e9c2caa3e6eb0e908e",
+    "url": "/static/js/main.439294e6.chunk.js"
+  },
+  {
+    "revision": "c81606e8a4beff6044ae",
+    "url": "/static/js/runtime-main.37c091aa.js"
+  }
+]);
