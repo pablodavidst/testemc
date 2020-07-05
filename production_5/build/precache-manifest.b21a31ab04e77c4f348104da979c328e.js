b0a8aafd7829273f62ae6d321162596982160817
pablo st <pablo.david.st@gmail.com>
2020-07-05 01:31:55 -0300

    nueva version para testing version 5

---
+++ production_5/build/precache-manifest.b21a31ab04e77c4f348104da979c328e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d7c314de88384469409809ead1e09be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf62e2c778a8ac9dabf2",
+    "url": "/static/css/main.6d7873a9.chunk.css"
+  },
+  {
+    "revision": "acba46edefa2ec902b5e",
+    "url": "/static/js/2.63fc2e3d.chunk.js"
+  },
+  {
+    "revision": "c2e47fc3395753f1500a13546dd93ceb",
+    "url": "/static/js/2.63fc2e3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf62e2c778a8ac9dabf2",
+    "url": "/static/js/main.76749a12.chunk.js"
+  },
+  {
+    "revision": "c81606e8a4beff6044ae",
+    "url": "/static/js/runtime-main.37c091aa.js"
+  }
+]);
