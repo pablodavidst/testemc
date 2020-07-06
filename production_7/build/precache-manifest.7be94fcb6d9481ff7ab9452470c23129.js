39a39c4d3b9d1f212191f5e16cdc6d225ef3b5a8
pablo st <pablo.david.st@gmail.com>
2020-07-06 01:16:29 -0300

    commit de version 7

---
+++ production_7/build/precache-manifest.7be94fcb6d9481ff7ab9452470c23129.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee8bf0fd70a99e721de4521b77a0c866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed7a64d6a15661143591",
+    "url": "/static/css/main.71f389d4.chunk.css"
+  },
+  {
+    "revision": "a42ed29f9643cec174fe",
+    "url": "/static/js/2.db13b6b0.chunk.js"
+  },
+  {
+    "revision": "c2e47fc3395753f1500a13546dd93ceb",
+    "url": "/static/js/2.db13b6b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed7a64d6a15661143591",
+    "url": "/static/js/main.e75efab0.chunk.js"
+  },
+  {
+    "revision": "c81606e8a4beff6044ae",
+    "url": "/static/js/runtime-main.37c091aa.js"
+  }
+]);
