ae68f89b9f607e8d7dee77f0d18e68a752e5ed2c
pablo st <pablo.david.st@gmail.com>
2020-07-06 18:22:28 -0300

    pasaje a preprod version 8

---
+++ production_8/build/precache-manifest.855899ce011c87ee30c47563d963b02c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f16e988816a36d51d5413c2f36faa5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d170eee06e0dab5d0d23",
+    "url": "/static/css/main.71f389d4.chunk.css"
+  },
+  {
+    "revision": "1dc1ce1dea8f309f23bc",
+    "url": "/static/js/2.799bce66.chunk.js"
+  },
+  {
+    "revision": "c2e47fc3395753f1500a13546dd93ceb",
+    "url": "/static/js/2.799bce66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d170eee06e0dab5d0d23",
+    "url": "/static/js/main.004b04dc.chunk.js"
+  },
+  {
+    "revision": "c81606e8a4beff6044ae",
+    "url": "/static/js/runtime-main.37c091aa.js"
+  }
+]);
