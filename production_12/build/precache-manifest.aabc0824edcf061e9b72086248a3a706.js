46af2ba71cc93787de612a072f4bb7c3165f0ce1
pablo st <pablo.david.st@gmail.com>
2020-08-08 23:03:34 -0300

    pasaje a produccion solo build version 12

---
+++ production_12/build/precache-manifest.aabc0824edcf061e9b72086248a3a706.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c9522ca1797efa163e0f1b61931e7e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa4bc7c1e313e02723df",
+    "url": "/static/css/main.7fe32620.chunk.css"
+  },
+  {
+    "revision": "2672549e3d08d2b5ff4e",
+    "url": "/static/js/2.31ec560f.chunk.js"
+  },
+  {
+    "revision": "c2e47fc3395753f1500a13546dd93ceb",
+    "url": "/static/js/2.31ec560f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa4bc7c1e313e02723df",
+    "url": "/static/js/main.a50afc3b.chunk.js"
+  },
+  {
+    "revision": "c81606e8a4beff6044ae",
+    "url": "/static/js/runtime-main.37c091aa.js"
+  },
+  {
+    "revision": "4a31e43720190dd77f0ebc45fbac4a67",
+    "url": "/static/media/logoemc.4a31e437.PNG"
+  }
+]);
